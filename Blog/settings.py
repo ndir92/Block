941f32ce3f07f9b9bfaa778641e09b737dafc7a0
ndir92 <mamadoundir2@gmail.com>
2023-01-05 17:22:24 +0000

    first commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 STATICFIELS_DIRS=[
     #STATIC_URL =os.path.join(BASE_DIR, 'staticfiles')
     BASE_DIR /'static',
